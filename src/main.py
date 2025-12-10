ca3761fbc3226917b5787b7b33ead1614cb32244
ghleprogramer <1xmnx1@gmail.com>
2025-12-10 18:07:42 +0300

    added market data

--- src/main.py
+++ src/main.py
@@ -1,2 +1,14 @@
 #main :D
 
+def main():
+	# read & print compaines of interest
+	
+	# prompt of additional compaines
+	
+	# scrape new announcments from https://www.saudiexchange.sa/wps/portal/saudiexchange/newsandreports/issuer-news/issuer-announcements/
+	
+	# summarise new announcments via an AI API
+	
+	# print out AI announcments summaries
+	
+	return
